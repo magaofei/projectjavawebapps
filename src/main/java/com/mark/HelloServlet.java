584db4e27b60c839ef097bb9c4727bc6f052b284
magaofei <mamian521@gmail.com>
2017-11-04 19:46:08 +0800

    chapter-3 hello-world

---
+++ src/main/java/com/mark/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.mark;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Hello, World!");
+//        System.out.println(getRequestURL());
+//        response.setStatus(200);
+//        System.out.println(request.getRequestURL());
+
+        
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet " + this.getServletName() + " has started. ");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has stopped");
+    }
+
+
+
+}
